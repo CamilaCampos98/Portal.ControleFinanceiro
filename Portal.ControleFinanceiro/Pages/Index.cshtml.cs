66b475106fbd33ee5d876bab7c1c30e900f523d5
Camila <mila.goncalves001@gmail.com>
2025-05-28 19:40:56 -0300

    Update Index page layout and API error handling
    Refactor `Index.cshtml` to remove the old welcome message and introduce a new style block with CSS variables. Added a main container with a welcome message in Portuguese and a lead paragraph for expense registration, along with an error alert for model errors.
    
    In `Index.cshtml.cs`, added `IConfiguration` to the constructor for configuration access. Replaced the synchronous `OnGet` method with an asynchronous `OnGetAsync` method to handle API requests, setting a success flag or capturing errors for display.

--- Portal.ControleFinanceiro/Pages/Index.cshtml.cs
+++ Portal.ControleFinanceiro/Pages/Index.cshtml.cs
@@ -1,20 +1,53 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Portal.ControleFinanceiro.Models;
+using System.Text;
+using System.Text.Json;
 
 namespace Portal.ControleFinanceiro.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
+        public string Mensagem { get; set; }
 
-        public void OnGet()
+        public bool Sucesso { get; set; }
+        public string? Erro { get; set; }
+
+        public async Task OnGetAsync()
         {
+            try
+            {
+                using var httpClient = new HttpClient();
+                var urlApi = _configuration["UrlApi"];
+                var url = $"{urlApi}Compra/Get";
 
+              
+
+                var response = await httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Sucesso = true;
+                    //Mensagem = $"⚠️ Gastos fixos para {Pessoa} gerados, preencha os valores.";
+                }
+                else
+                {
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    Erro = $"❌ Erro ao salvar na API: {errorContent}";
+                }
+            }
+            catch (Exception ex)
+            {
+                Mensagem = $"❌ Erro na comunicação com a API: {ex.Message}";
+            }
         }
     }
 }
